a9af0d7a973340b99e4a67fede35d6adf4a75eb2
inugato <longlamloi97@gmail.com>
2017-09-24 09:23:30 +0700

    updated

---
+++ Bai1.java
@@ -0,0 +1,18 @@
+import java.utli.*;
+class Bai1 
+{
+	public static void main(String []args)
+	{
+	Scanner sc = new Scanner(System.in);
+	    int S=0;
+	    int n=0;
+	System.out.println("inter value: ");
+	System.out.println("\n");
+	n = sc.nextInt();
+	    for(int i=0;i<n;i++)
+	    {
+		S = S +i;
+	    }
+	    System.out.println("S: ",+S);
+	}
+}
