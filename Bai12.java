eef4dda138dc442bccca1f4ec689aead55ff0910
inugato <longlamloi97@gmail.com>
2017-10-04 20:16:48 +0700

    updated

--- Bai12.java
+++ Bai12.java
@@ -1,5 +1,20 @@
 import java.util.*;
 public class Bai12
-{
-	static int 
+{	
+	static double power = 0;
+	static double x = 0;
+	public static void main(String []args)
+	{
+		System.out.println("Enter X: ");
+		Scanner sc = new Scanner(System.in);
+		double x = sc.nextDouble();
+		System.out.println("Enter n: ");
+		int n = sc.nextInt();
+		for(double i=1;i<=n;i++)
+		{
+			power = Math.pow(x,i);
+			x = x + power;
+		} 
+		System.out.println("Sum: ",+x);
+	}
 }
